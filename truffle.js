d78c55c30f9f9e3d17db6adcba5a4f0d847a39dd
Rich Noordam <richard.noordam@gmail.com>
2018-08-24 19:17:13 -0700

    Rinkeby setup removal

--- truffle.js
+++ truffle.js
@@ -8,11 +8,5 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     }
-  },rinkeby: {
-    host: "localhost", // Connect to geth on the specified
-    port: 8545,
-    from: "0x839c18df17236382f8832d9ab5ef3facafbac891", // default address to use for any transaction Truffle makes during migrations
-    network_id: 4, 
-    gas: 4612388 
-    }
+  }
 }
